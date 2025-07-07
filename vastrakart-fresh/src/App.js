d7d23ee46ae8118a869fac065bca2bcb9afaf175
Lakshmicharitha <lakshmicharitha10072004@gmail.com>
2025-07-07 18:43:07 +0530

    react

--- vastrakart-fresh/src/App.js
+++ vastrakart-fresh/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import CartPage from "./components/CartPage";
+import WishlistPage from "./components/WishlistPage";
+import CheckoutForm from "./components/CheckoutForm";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const hash = window.location.hash;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>VastraKart</h1>
+      <Navbar />
+
+      {hash === "#cart" ? (
+        <CartPage />
+      ) : hash === "#wishlist" ? (
+        <WishlistPage />
+      ) : hash === "#checkout" ? (
+        <CheckoutForm />
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
-}
+};
 
 export default App;
